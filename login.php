a9357d67625c13100be48b3c3f88fd3b9a67d496
moses-gangipogu <mosesgangipogu@gmail.com>
2016-12-06 17:03:57 +0000

    login

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+   $username = $_POST['username'];
+   $password = $_POST['password']; 
+   if ($username == "admin" && $password == "password"){
+     echo "Login SuccessFull"
+   } else {
+      echo "Login UnSuccessFull"
+   }
+?>
+<html>
+<head>
+<title>Login Page</title>
+</head>
+   <body bgcolor = "#FFFFFF">
+      <form action = "" method = "post">
+      <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+      <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+      <input type = "submit" value = " Submit "/><br />
+      </form>
+   </body>
+</html>
